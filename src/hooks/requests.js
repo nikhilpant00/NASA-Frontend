9afd33825602b3b8bf75c178214da15484d414e4
nikhilpant00 <105990262+nikhilpant00@users.noreply.github.com>
2023-09-06 12:40:24 +0530

    final

---
+++ src/hooks/requests.js
@@ -0,0 +1,49 @@
+const API_URL = "http://localhost:8000";
+
+// Load planets and return as JSON.
+async function httpGetPlanets() {
+  const response = await fetch(`${API_URL}/planets`);
+  const body = await response.json();
+  return body;
+}
+
+// Load launches, sort by flight number, and return as JSON.
+async function httpGetLaunches() {
+  const response = await fetch(`${API_URL}/launches`);
+  const body = await response.json();
+  return body.sort((a, b) => {
+    return a.flightNumber - b.flightNumber;
+  });
+}
+
+// Submit given launch data to launch system.
+async function httpSubmitLaunch(launch) {
+  try {
+    return await fetch(`${API_URL}/launches`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(launch),
+    });
+  } catch {
+    return {
+      ok: false,
+    };
+  }
+}
+
+// Delete launch with given ID.
+async function httpAbortLaunch(id) {
+  try {
+    return await fetch(`${API_URL}/launches/${id}`, {
+      method: "delete",
+    });
+  } catch {
+    return {
+      ok: false,
+    };
+  }
+}
+
+export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
